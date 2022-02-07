3d8d2dbd3f569ae6039cbca15dfc7c1649fb083a
Aaron Urkin <aaron.urkin@1beat.com>
2022-02-07 18:50:06 +0200

    Users test list have been added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = []
+PASSWORD = "123456"
+
+10.times.each do |i|
+    user_number = i + 1
+
+    user = User.create({
+        password: PASSWORD,
+        password_confirmation: PASSWORD,
+        nickname: "#{user_number}_nickname",
+        email: "test#{user_number}@test.mail",
+        full_name: "#{user_number}_first_name #{user_number}_last_name"
+    })
+    users.append(user)
+end
